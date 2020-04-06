41a6756ade3abc5e6752af4e813fe4b5e9440cb4
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-04-07 02:28:36 +0900

    Change Nav a bit but remained animation issue

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import { NavLink } from 'react-router-dom';
+import './Nav.css';
 
-const Nav = () => {
-    return(
-        <nav>
-            <ul>
-                <li><NavLink exact={true} activeStyle={{ fontSize:24, fontWeight:'bold'}} to="/">Home</NavLink></li>
-                <li><NavLink activeStyle={{ fontSize:24}} to="/about">소개</NavLink></li>
-                <li><NavLink activeStyle={{ fontSize:24}} to="/posts">Posts</NavLink></li>
-                <li><NavLink to="/admin">Admin</NavLink></li>
-            </ul>
-        </nav>
-    );
+class Nav extends React.Component {
+
+    state = {
+        navActive: false,
+    }
+
+    render(){
+        const class1 = ["burger"];
+        const class2 = ["links"];
+        if(this.state.navActive) {
+            class1.push('nav-active toggle');
+            class2.push('nav-fade');
+        }
+        return(
+            <nav>
+                <div className="logo">
+                    <NavLink to="/" exact={true} ><h3>Reactive TS</h3></NavLink>
+                </div>
+                <ul className="nav-links" >
+                    <li className={class2.join(' ')}><NavLink activeStyle={{ fontSize:18}} to="/about">About</NavLink></li>
+                    <li className={class2.join(' ')}><NavLink activeStyle={{ fontSize:18}} to="/proj">Project</NavLink></li>
+                    <li className={class2.join(' ')}><NavLink activeStyle={{ fontSize:18}} to="/posts">Posts</NavLink></li>
+                    <li className={class2.join(' ')}><NavLink activeStyle={{ fontSize:18}} to="/admin">Admin</NavLink></li>
+                </ul>
+                <div className={class1.join(' ')} onClick={this.listenToBurger.bind(this)}>
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+                </div>
+            </nav>
+            
+        );
+    }
+
+    listenToBurger: any = (e : MouseEventHandler<HTMLElement>) =>{
+        // e.preventDefault();
+        // const nav = document.querySelector('.nav-links');
+        const navLinks = document.querySelectorAll('.nav-links li');
+
+        this.setState({navActive:!this.state.navActive});
+    
+        navLinks.forEach((link, idx) => {
+            // const fade=`navLinkFade 0.5s ease forwards ${idx/3.7725}s`;
+        });    
+    }
 }
 
 export default Nav;
