497f1ee3b53041f8447c2216fa5f89e65c0c4298
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-04-06 21:09:32 +0900

    Add design to 404 error page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
-    return (
-      <h3>Not Found</h3>
-    )
+    
+  return (
+    <div>
+      <div className="error-page">
+        <h1 data-h1="404">404</h1>
+        <p data-p="NOT FOUND">NOT FOUND</p>
+      </div>
+      <div id="particles-js"></div>
+    </div>
+  )
 }
 
 export default NotFound;
