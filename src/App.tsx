5dfbcd4654e136c3fd02119d2ad1f6d9797b90f6
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-04-06 18:35:46 +0900

    Add simple Redux example

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,48 @@
 import React from 'react';
+import Nav from './components/Nav';
+import Home from './components/Home';
 import About from './components/About';
 import Posts from './components/Posts';
 import Admin from './components/Admin';
 import NotFound from './components/NotFound';
-import { Route, Link } from 'react-router-dom'; //필수
-import { RouteComponentProps, Switch, Redirect, NavLink } from 'react-router-dom';
-// import { StaticRouter } from 'react-router'
+import { Route, Switch, Redirect } from 'react-router-dom'; //필수
+import { Store } from 'redux';
+import { addAge } from './index';
 
-// class App extends React.Component<{}, null> { //Generics<props, state>
-function App() {
-  return (
-    <div> {/* <StaticRouter> */}
-      {/* <nav>
-        <ul>
-          <li><NavLink exact={true} activeStyle={{ fontSize:24, fontWeight:'bold'}} to="/">Home</NavLink></li>
-          <li><NavLink activeStyle={{ fontSize:24}} to="/about">소개</NavLink></li>
-          <li><NavLink activeStyle={{ fontSize:24}} to="/posts">Posts</NavLink></li>
-          <li><NavLink to="/admin">Admin</NavLink></li>
-        </ul>
-      </nav> */}
-      <Switch>
-        <Route exact={true} path="/" component={Home} />
-        <Route path="/about" component={About} /> {/* render={()=><h3>소개</h3>} */}
-        <Redirect from="/intro" to="/about" />
-        <Route exact={true} path="/posts" component={Posts} />
-        {/* <Route exact={true} path="/posts/:postId" component={Post} /> */}
-        <Route path="/admin" component={Admin} />
-        <Route component={NotFound} />
-        <Redirect from="*" to="/" />
-      </Switch>
-      </div> /*</StaticRouter> 앱을 감싸도록 써주면 어디서든지 라우터를 쓸수 있는것 */
-  );
+type AppProps = {
+  store: Store<{age: number}>
 }
 
-const Home = () => {
-  return (
-    <div>
-      <h2>Home</h2>
-      <p><Link to="/">Home</Link></p>
-      <p><Link to="/about">About</Link></p>
-      <p><Link to="/posts">Posts</Link></p>
-      <p><Link to="/admin">Admin</Link></p>
-    </div>
-  )
+class App extends React.Component<AppProps, {}> { //Generics<props, state>
+// function App() {
+
+  render(){
+
+    const store = this.props.store;
+    const state = store.getState();
+
+    return (
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact={true} path="/" component={Home} />
+          <Route path="/about" component={About} /> {/* render={()=><h3>소개</h3>} */}
+          <Redirect from="/intro" to="/about" />
+          <Route exact={true} path="/posts" component={Posts} />
+          {/* <Route exact={true} path="/posts/:postId" component={Post} /> */}
+          <Route path="/admin" component={Admin} />
+          <Route component={NotFound} />
+          <Redirect from="*" to="/" />
+        </Switch>
+        <div>
+        {state.age}
+        <button onClick={()=>{
+          store.dispatch(addAge())
+        }}>한해가지났다</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,44 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-  ,document.getElementById('root')
-);
+import {createStore, Store} from 'redux';
+// import { Provider } from 'react-redux'
 
+const ADD_AGE = 'ADD_AGE';
+
+export function addAge(){
+  return {
+    type : ADD_AGE
+  }
+}
+
+function ageApp(state:{age:number}={age:35}, action: {type: 'ADD_AGE'}): {age: number} {
+  if(action.type===ADD_AGE) {
+    return {
+      age: state.age + 1
+    }
+  }
+  return state;
+}
+
+const store: Store<{age:number;}> = createStore(ageApp);
+// const store = createStore<{ age: number; }>(ageApp);
+
+store.subscribe(render)
+
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App store={ store }/>
+      </BrowserRouter>
+    </React.StrictMode>
+    ,document.getElementById('root')
+  );
+}
+
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
