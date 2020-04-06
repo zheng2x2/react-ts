c547fdb2b681e3941b9eb733c7e8901bd7a5cc21
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-04-06 21:37:24 +0900

    Correct css

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -9,7 +9,7 @@ const NotFound = () => {
         <h1 data-h1="404">404</h1>
         <p data-p="NOT FOUND">NOT FOUND</p>
       </div>
-      <div id="particles-js"></div>
+      {/* <div id="particles-js"></div> */}
     </div>
   )
 }
