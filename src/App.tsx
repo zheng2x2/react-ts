c4c7859196859661a3a197132ef867e24a5d8406
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-04-08 22:49:47 +0900

    Change redux to react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -7,43 +7,56 @@ import Admin from './components/Admin';
 import NotFound from './components/NotFound';
 import Footer from './components/Footer';
 import { Route, Switch, Redirect } from 'react-router-dom'; //필수
-import { Store } from 'redux';
 import { addAge } from './index';
+import { connect } from 'react-redux';
 
 type AppProps = {
-  store: Store<{age: number}>
+  // store: Store<{age: number}>
+  age: number;
+  onAddClick(): void;
 }
 
-class App extends React.Component<AppProps, {}> { //Generics<props, state>
-
-  render(){
-
-    const store = this.props.store;
-    const state = store.getState();
-
+// class App extends React.Component<AppProps, {}> { //Generics<props, state>
+//   render(){
+const App: React.SFC<AppProps> = (props) => {
     return (
       <div>
         <Nav />
         <Switch>
           <Route exact={true} path="/" component={Home} />
           <Route path="/about" component={About} /> {/* render={()=><h3>소개</h3>} */}
-          <Redirect from="/intro" to="/about" />
-          <Route exact={true} path="/posts" component={Posts} />
-          {/* <Route exact={true} path="/posts/:postId" component={Post} /> */}
+          <Redirect from="/intro" to="/about" /> {/* depracated */}
+          <Route exact={true} path="/posts" component={Posts} /> {/* <Route exact={true} path="/posts/:postId" component={Post} /> */}
           <Route path="/admin" component={Admin} />
           <Route component={NotFound} />
           <Redirect from="*" to="/" />
         </Switch>
         <div>
-        {state.age}
-        <button onClick={()=>{
-          store.dispatch(addAge())
-        }}>한해가지났다</button>
+          {props.age}
+          <button onClick={()=>{ props.onAddClick() }}>한해가지났다</button>
         </div>
         <Footer/>
       </div>
     );
-  }
 }
 
-export default App;
+// 이 함수는 store.getState() 한 state 를 연결한(connect) App 컴포넌트의 어떤 props 로 줄 것인지를 리턴
+// 그래서 이 함수의 리턴이 곧 App 컴포넌트의 AppProps 의 부분집합이어야 한다.
+const mapStateToProps = ( state: {age: number} ) => {
+  return {
+    age: state.age
+  }
+}
+// 이 함수는 store.dispatch(액션)을 연결한(connect) App컴포넌트의 어떤 props로 줄 것인지를 리턴
+// 그래서 이 함수의 리턴이 곧 App 컴포넌트의 AppProps의 부분집합이어야 한다.
+const mapDispatchToProps = (dispatch: Function) => {
+  return {
+    onAddClick: () => {
+      dispatch( addAge() );
+    }
+  }
+}
+const AppContainer = connect(
+  mapStateToProps, mapDispatchToProps
+)(App);
+export default AppContainer;
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 
 import {createStore, Store} from 'redux';
+import {Provider} from 'react-redux';
 
 const ADD_AGE = 'ADD_AGE';
 
@@ -22,24 +23,31 @@ function ageApp(state:{age:number}={age:35}, action: {type: 'ADD_AGE'}): {age: n
   }
   return state;
 }
+const initialState = (window as any).__INITIAL_STATE__;
+//서버에서 받은 초기값으로 스토어 만들기
+const store: Store<{age:number;}> = createStore(ageApp, initialState);
 
-const store: Store<{age:number;}> = createStore(ageApp);
-// const store = createStore<{ age: number; }>(ageApp);
+// class Provider extends React.Component<{store: Store<{age:number}>; children: JSX.Element}> {
+//   static childContextTypes = {
+//     store: PropTypes.object
+//   }
+//   getChildComtext() {
+//     return { store: this.props.store }
+//   }
+//   render(){ return this.props.children; }
+// }
 
-store.subscribe(render)
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+  ,document.getElementById('root')
+);
 
-function render() {
-  ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App store={ store }/>
-      </BrowserRouter>
-    </React.StrictMode>
-    ,document.getElementById('root')
-  );
-}
-
-render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
